f4822efc9e23992276da2bf40730c88898d0f48b
BENDRISS <elm.bendriss@gmail.com>
2021-07-16 19:14:28 +0100

    Add a program version using MySQL connector

---
+++ programUsingMySQL.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+con = mysql.connector.connect(user="", password="", host="", database="")
+
+cursor = con.cursor()
+
+word = input("Enter a word : ")
+
+req = cursor.execute("select * from Dictionnary where expression = '%s'" % word)
+
+results = cursor.fetchall()
+
+if results:
+    for result in results:
+        print(result[1])
+else:
+    print("No such word")

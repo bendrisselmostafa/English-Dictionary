81c5d636473ac2408b5a151193291cbcbb1dab42
BENDRISS <elm.bendriss@gmail.com>
2021-07-16 17:41:55 +0100

    Fix Input

--- program.py
+++ program.py
@@ -4,8 +4,11 @@ data = json.load(open("data.json"))
 
 
 def definition():
-    word = input("Enter a word: ")
-    return data[word]
+    word = input("Enter a word: ").lower()
+    if word in data:
+        return data[word]
+    else:
+        return "No such word"
 
 
 print(definition())

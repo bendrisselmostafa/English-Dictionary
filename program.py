5dfddc2e8ae91707d474f543ba832a03aab67285
BENDRISS <elm.bendriss@gmail.com>
2021-07-16 18:37:43 +0100

    Add recommendation of the best match

--- program.py
+++ program.py
@@ -1,4 +1,5 @@
 import json
+import difflib
 
 data = json.load(open("data.json"))
 
@@ -7,6 +8,20 @@ def definition():
     word = input("Enter a word: ").lower()
     if word in data:
         return data[word]
+    elif len(difflib.get_close_matches(word, data.keys())) > 0:
+        close_word = difflib.get_close_matches(word, data.keys())[0]
+        choice = input(
+            "Did you mean %s instead ? Y = Yes | N = No : " % close_word
+        ).lower()
+
+        while not (choice == "y" or choice == "n"):
+            print("Invalid input")
+            choice = input(
+                "Did you mean %s instead ? Y = Yes | N = No : " % close_word
+            ).lower()
+        if choice == "y":
+            return data[close_word]
+        return "Please check Again"
     else:
         return "No such word"
 
